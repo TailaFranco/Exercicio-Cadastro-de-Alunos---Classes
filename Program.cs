89278eb6509e53039af9474be9581590ffa99bb6
DEV <dev@info.local>
2020-12-02 07:56:19 -0300

    Inicio

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Aula_02_12_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+           Console.WriteLine("Sistema de Cadastro de Alunos");
+            Aluno objetoAluno = new Aluno();
+        
+        }
+    }
+}
---
+++ classes/Aluno.cs
@@ -0,0 +1,16 @@
+namespace Aula_02_12_2020
+{
+//     Vamos desenvolver um programa que cadastre e mostre um aluno. Criando uma classe aluno com as seguintes
+// propriedades: Nome, Curso, Idade, RG, Bolsista (boolean), Média Final e Valor da Mensalidade. Também teremos os métodos: VerMediaFinal() e VerMensalidade(), caso seja bolsista faremos o cálculo da mensalidade.
+    public class Aluno
+    {
+        public string nome;
+        public string curso;
+        public string rg;
+        public int idade;
+        public bool bolsista;
+        public float médiaFinal;
+        public float valorMensalidade;
+        
+    }
+}
